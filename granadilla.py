0b564eb621f9e147013eb16d4fffaa43327b8de2
danalejos <sumigame98@gmail.com>
2023-04-22 12:44:04 -0500

    Keooo

--- granadilla.py
+++ granadilla.py
@@ -1,9 +1,20 @@
 class granadilla:
 
     def _init_(self):
-        self.nombre=None
-        self.cantidad=None
-        self.precio=None
-        self.vitaminaAportada=None
+        self.__nombre=None
+        self.__cantidad=None
+        self.__precio=None
+        self.__vitaminaAportada=None
+
+        #Declarando el metodo GET (Obtengo/muestro un atributo)
+        @property
+        def  nombre (self):
+            return self.__nombre
+        #Declarando el metodo SET (configuro, llevo un valor a un atributo)
+        @nombre.setter
+        def nombre(self,dato):
+            self.__nombre=dato
+
+
     def agregarFruta(self):
         print ("Se raspa y se tira a lo mas despreciable")
---
+++ main.py
@@ -0,0 +1,5 @@
+from granadilla import granadilla
+
+objeto = granadilla()
+objeto.nombre="Granadilla weed" #SET
+print (objeto.nombre) #GET
