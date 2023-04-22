92557b99005bdccc5cea9d6aa79ec7e95f1b42a1
Your Name <you@example.com>
2023-04-22 12:54:07 -0500

    NT10 se crean getter and setter

---
+++ main.py
@@ -0,0 +1,16 @@
+from Corozo import Corozo
+
+objeto=Corozo()
+
+objeto.nombre='Corozo Acrocomia aculeata'
+
+objeto.cantidad='5 Kilos'
+
+objeto.precio='50mil'
+
+objeto.vitaminaAportada='Vitaminas A,B,C y E'
+
+print(objeto.nombre)
+print(objeto.cantidad)
+print(objeto.precio)
+print(objeto.vitaminaAportada)
