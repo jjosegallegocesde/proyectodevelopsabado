4c7b5da2ff0dc9a8ff7f91ddef905e3a4ab37c54
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-04-22 12:55:59 -0500

    NT10-metodos get y set

--- Chilacuan.py
+++ Chilacuan.py
@@ -1,9 +1,58 @@
 class Chilacuan: 
     def __init__(self):
-        self.nombre = None
-        self.cantidad = None
-        self.precio = None
-        self.vitaminaAportada = None
+        self.__nombre = None
+        self.__cantidad = None
+        self.__precio = None
+        self.__vitaminaAportada = None
+    
+    # NOMBRE     
+    # declarar metodo get  
+    @property 
+    def nombre(self):
+        return self.__nombre
+    
+    # declarando metodo set 
+    
+    @nombre.setter
+    def nombre(self, dato):
+        self.__nombre = dato
+    
+    # CANTIDAD     
+    # declarar metodo get  
+    @property 
+    def cantidad(self):
+        return self.__cantidad
+    
+    # declarando metodo set 
+    
+    @cantidad.setter
+    def cantidad(self, dato):
+        self.__cantidad = dato
         
+    # PRECIO     
+    # declarar metodo get  
+    @property 
+    def precio(self):
+        return self.__precio
+    
+    # declarando metodo set 
+    
+    @precio.setter
+    def precio(self, dato):
+        self.__precio = dato
+        
+    # VITAMINA      
+    # declarar metodo get  
+    @property 
+    def vitaminaAportada(self):
+        return self.__vitaminaAportada
+    
+    # declarando metodo set 
+    
+    @vitaminaAportada.setter
+    def vitaminaAportada(self, dato):
+        self.__vitaminaAportada = dato
+    
     def agregarFruta(self):
         print("se pica y se echa")
+    
---
+++ main.py
@@ -0,0 +1,12 @@
+from Chilacuan import Chilacuan
+
+objeto = Chilacuan()
+
+objeto.nombre = "chilacuan" 
+print(objeto.nombre) 
+objeto.cantidad = "12" 
+print(objeto.cantidad) 
+objeto.precio = "1200" 
+print(objeto.precio) 
+objeto.vitaminaAportada = "A" 
+print(objeto.vitaminaAportada) 
