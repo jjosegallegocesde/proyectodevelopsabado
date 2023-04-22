1bbcf5e07a00ba99b2d1164786e6836b027c435b
lina <linarios512@hotmail.com>
2023-04-22 11:25:10 -0500

    NT10 Se creaa clase fresa

---
+++ Fresa.py
@@ -0,0 +1,9 @@
+class Fresa:
+    def __init__(self):
+        self.nombre = None
+        self.cantidad = None
+        self.precio = None
+        self.vitaminaAportada = None
+
+    def agregarFruta(self):
+        print("Se pica y se macera con azucar morena")
