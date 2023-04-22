d7e1b3561d7c429f35e6b9a7e7c6d7bb2004b18f
jotapeco988 <89583628+jotapeco988@users.noreply.github.com>
2023-04-22 12:55:08 -0500

    NT10 Se hace modificancion en el archivo, get and set

---
+++ main.py
@@ -0,0 +1,5 @@
+from sapote import sapote
+
+objeto = sapote()
+objeto.nombre="Sapote de antioquia"
+print(objeto.nombre)
--- sapote.py
+++ sapote.py
@@ -1,9 +1,24 @@
 class sapote:
     def __init__(self):
-        self.nombre=None
-        self.cantidad=None
-        self.precio=None
-        self.vitaminaApor=None
+        self.__nombre=None
+        self.__cantidad=None
+        self.__precio=None
+        self.__vitaminaApor=None
+    
+    @property
+    def nombre (self):
+        return self.__nombre
+    @property
+    def cantidad(self):
+        return self.__cantidad
+    
+    @nombre.setter
+    def nombre(self,dato):
+        self.__nombre=dato
+    
+    @cantidad.setter
+    def cantidad(self,dato):
+        self.cantidad.dato
 
     def agregarFruta(self):
         print("se pica y se macera con azucar morena")
