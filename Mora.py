8cb6985da659739b3780f93e8cab071e1f91892f
dicojo <diniercj@gmail.com>
2023-04-22 11:25:00 -0500

    NT10 se crea clase Mora

---
+++ Mora.py
@@ -0,0 +1,10 @@
+class Mora:
+
+    def __init__(self):
+        self.nombre=None
+        self.cantidad=None
+        self.precio=None
+        self.vitaminaAportada=None
+    
+    def agregarFruta(self):
+        print("se pica y se macera con azucar morena") 
