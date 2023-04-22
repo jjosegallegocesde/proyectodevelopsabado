ee605361e96bb62be59e4b36f4f6466e0730f5d9
euclidesseg <euder0206@gmail.com>
2023-04-22 11:33:40 -0500

    NT10 se crea la clase Kiwi

---
+++ kiwi.py
@@ -0,0 +1,8 @@
+class Kiwi():
+    def public __init__(self):
+        self.nombre = None
+        self.vitamina = None
+        self.cantidad = None
+
+    def agregarFruta(self):
+        print("se pica y se macera con azucar")
