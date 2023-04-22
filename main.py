7be8f848277bb9f2743326135cde4727ba424aef
wilyCesde <wruizza@cesde.net>
2023-04-22 12:57:01 -0500

    agrgego get set

---
+++ main.py
@@ -0,0 +1,13 @@
+from Albaricoque import Albaricoque
+
+
+
+
+
+objeto = Albaricoque()
+objeto.nombre='Albaricoque' #set
+print(objeto.nombre) #get
+
+objeto.cantidad=10 #set
+print(objeto.cantidad) #get
+
