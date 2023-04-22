b230de528bc85bca581e7369f14185753fc00a97
cuatrotales <jfquinterogu14@gmail.com>
2023-04-22 11:33:29 -0500

    NT10 - se crea clase Alquejenje

---
+++ Alquejenje.py
@@ -0,0 +1,9 @@
+class Alquejenje:
+    def __init__(self):
+        self.nombre = None
+        self.cantidad = None
+        self.precio=None
+        self.vitaminaAportada=None
+
+    def agregarFruta(self):
+        print(f'Se pica y se macera con azucar morena')
