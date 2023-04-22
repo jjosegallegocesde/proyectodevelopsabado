ac7ce3bf4a0d901825a38bfda76b80dd90aa1ed5
DUVAN100 <duvanvivas100@gmail.com>
2023-04-22 11:33:34 -0500

    se crea clase fruta

---
+++ Durazno.py
@@ -0,0 +1,11 @@
+class Durazno:
+    def __init__(self):
+        self.nombre = None
+        self.cantidad = None
+        self.precio = None
+        self.vitanmina = None
+        
+    def agregarFrua(self):
+        print("se pica y se hecha se macera con azucar")
+        
+   
