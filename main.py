a8364d8bb42c181e82bb535ce7cc2914873bba4c
wilyCesde <wruizza@cesde.net>
2023-04-22 13:10:07 -0500

    resivo cambios

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ print(objeto.nombre) #get
 objeto.cantidad=10 #set
 print(objeto.cantidad) #get
 
+from Alquejenje import Alquejenje
+
+objeto=Alquejenje()
+
+objeto.nombre="Alquejenje"
+print(objeto.nombre)
+
+objeto.cantidad=1
+print(objeto.cantidad)
+
+objeto.precio=5000
+print(objeto.precio)
+
+objeto.vitaminaAportada=5000
+print(objeto.vitaminaAportada)
