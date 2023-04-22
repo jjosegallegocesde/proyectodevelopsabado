eeabd7dc7b05e3bc347a8a1b82e454ee7aef068b
cuatrotales <jfquinterogu14@gmail.com>
2023-04-22 12:55:07 -0500

    NT10 - Se crean getters y setters

--- Alquejenje.py
+++ Alquejenje.py
@@ -1,9 +1,42 @@
 class Alquejenje:
     def __init__(self):
-        self.nombre = None
-        self.cantidad = None
-        self.precio=None
-        self.vitaminaAportada=None
+        self.__nombre = None
+        self.__cantidad = None
+        self.__precio=None
+        self.__vitaminaAportada=None
+    
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self, data):
+        self.__nombre=data
 
+    @property
+    def cantidad(self):
+        return self.__cantidad
+    
+    @cantidad.setter
+    def cantidad(self, data):
+        self.__cantidad=data
+
+    @property
+    def precio(self):
+        return self.__precio
+    
+    @precio.setter
+    def precio(self, data):
+        self.__precio=data
+
+    @property
+    def vitaminaAportada(self):
+        return self.__vitaminaAportada
+    
+    @vitaminaAportada.setter
+    def vitaminaAportada(self, data):
+        self.__vitaminaAportada=data
+
+    
     def agregarFruta(self):
         print(f'Se pica y se macera con azucar morena')
---
+++ main.py
@@ -0,0 +1,15 @@
+from Alquejenje import Alquejenje
+
+objeto=Alquejenje()
+
+objeto.nombre="Alquejenje"
+print(objeto.nombre)
+
+objeto.cantidad=1
+print(objeto.cantidad)
+
+objeto.precio=5000
+print(objeto.precio)
+
+objeto.vitaminaAportada=5000
+print(objeto.vitaminaAportada)
