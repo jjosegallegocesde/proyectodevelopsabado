42d7ef719baaca215fae05a1183f79d13eb75a2a
DanielVasquezCESDE <dontblamemydata@gmail.com>
2023-04-22 12:58:00 -0500

    NT10 Segundo versionamiento

--- Apple.py
+++ Apple.py
@@ -1,9 +1,45 @@
-class Apple:
+class Apple:              #para poner un atributo privado es la siguiente forma __
     def __init__(self):
-      self.nombre=None
-      self.cantidad=None
-      self.precio=None
-      self.vitaminaAportada=None
+      self.__nombre=None
+      self.__cantidad=None
+      self.__precio=None
+      self.__vitaminaAportada=None
+
+    #DECLARANDO EL METODO GET (OBTENGO/MUESTRO UN ATRIBUTO)
+    @property
+    def nombre(self):
+       return self.__nombre
+    
+    @property
+    def cantidad(self):
+       return self.__cantidad
+    
+    @property
+    def precio(self):
+       return self.__precio
+    
+    @property
+    def vitaminaAportada(self):
+       return self.__vitaminaAportada
+    
+    #DECLANDO EL METODO SET (CONFIGURO, LLEVO UN VALOR A UN ATRIBUTO.)
+
+    @vitaminaAportada.setter
+    def vitaminaAportada(self,dato):
+       self.__vitaminaAportada=dato
+
+    @precio.setter
+    def precio(self,dato):
+       self.__precio=dato
+
+    @cantidad.setter
+    def cantidad(self,dato):
+       self.__cantidad=dato
+
+    @nombre.setter
+    def nombre(self,dato):
+       self.__nombre=dato
+
 
     def agregarFruta():
        print('Se pica y se macera con azucar morena',)
---
+++ main.py
@@ -0,0 +1,13 @@
+from Apple import Apple
+
+objeto=Apple()
+objeto.nombre='Manzanas de colombia' #ESTE ES EL SET
+objeto.cantidad=9
+objeto.precio=25.000
+objeto.vitaminaAportada='Excelente fruta...'
+
+print(objeto.nombre)#ESTE ES GET
+print(objeto.cantidad)
+print(objeto.precio)
+print(objeto.vitaminaAportada)
+
