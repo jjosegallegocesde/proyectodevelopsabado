843e49134fe9c8a9692729d2b4a62b1a2731b749
dayana2806 <dayismo12345@gmail.com>
2023-04-22 12:54:12 -0500

    NT10 se crea getter y setters

---
+++ main.py
@@ -0,0 +1,11 @@
+from pitahaya import pitahaya
+
+objeto=pitahaya()
+objeto.nombre="pitahaya europea" #SET
+print(objeto.nombre) #GET
+objeto.cantidad="200 pitahayas"
+print(objeto.cantidad)
+objeto.precio="100.000"
+print(objeto.precio)
+objeto.vitaminaAportada
+print("vitanina c")
--- pitahaya.py
+++ pitahaya.py
@@ -1,9 +1,44 @@
 class pitahaya:
     def __init__(self):
-        self.nombre=None
-        self.cantidad=None
-        self.precio=None
-        self.vitaminaAportada=None
+        self.__nombre=None
+        self.__cantidad=None
+        self.__precio=None
+        self.__vitaminaAportada=None
+
+#DECLARANDO METODO GET (OBTEMGO/MUESTRO UN ATRIBUTO)
+    @property
+    def nombre(self):
+        return self.__nombre
     
+    #DECLARANDO EL METODO SELF (CONFIGURO,LLEVO UN VALOR A UN ATRIBUTO)
+    @nombre.setter
+    def nombre(self,dato):
+        self.__nombre = dato
+
     def agregarFruta(self):
         print("se pica y se macera con azucar")
+
+    @property
+    def cantidad(self):
+        return self.__cantidad
+    
+    @cantidad.setter
+    def cantidad(self,dato):
+        self.__cantidad = dato
+
+    @property
+    def precio(self):
+        return self.__precio
+    
+    @precio.setter
+    def precio(self,dato):
+        self.__precio= dato
+
+    @property
+    def vitaminaAportada(self):
+        return self.__vitaminaAportada
+    
+    @vitaminaAportada.setter
+    def vitaminaAportada(self,dato):
+        self.__vitaminaAportada = dato
+
