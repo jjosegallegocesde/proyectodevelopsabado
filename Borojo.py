e619f1b789a6b44c0bf449fc9f4a101515fe4604
cuatrotales <jfquinterogu14@gmail.com>
2023-04-22 13:03:04 -0500

    NT10 se crea getter y setter

--- Borojo.py
+++ Borojo.py
@@ -1,8 +1,44 @@
 class  Borojo:
     def __init__(self):
-        self.nombre=None
-        self.cantidad = None
-        self.precio = None
-        self.vitaminaaportada = None
+        self.__nombre=None
+        self.__cantidad = None
+        self.__precio = None
+        self.__vitaminaaportada = None
+
+    #declarando el metodo GET  (OBTENGO /MUESTRA UN ATRIBUTO)
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @property
+    def cantidad(self):
+        return self.__cantidad
+    
+    @property
+    def precio(self):
+        return self.__precio
+    
+    @property
+    def vitaminaaportada (self):
+        return self.__vitaminaaportada
+    
+   
+    #declarando el metodo SET  (CONFIGURO ,LLEVO UN VALOR A UN ATRIBUTO)
+    @nombre.setter
+    def nombre(self,dato):
+        self.__nombre = dato
+
+    @cantidad.setter
+    def cantidad(self,dato):
+        self.__cantidad = dato    
+
+    @precio.setter
+    def precio (self,dato):
+        self.__precio= dato 
+
+    @vitaminaaportada.setter
+    def vitaminaaportada  (self,dato):
+        self.__vitaminaaportada = dato       
+
     def agregarFruta(self):
         print("se le quita la bolsa y le echa leche")
---
+++ main.py
@@ -0,0 +1,18 @@
+from Borojo import  Borojo
+
+objeto=Borojo()
+objeto.nombre= "Borojo afrodiciaco" #set
+print (objeto.nombre)#get
+
+objeto=Borojo()
+objeto.cantidad =" 150"
+print(objeto.cantidad)
+
+objeto=Borojo()
+objeto.precio  =" 15000"
+print(objeto.precio )
+
+objeto=Borojo()
+objeto.vitaminaaportada  =" vitamina D"
+print(objeto.vitaminaaportada )
+
