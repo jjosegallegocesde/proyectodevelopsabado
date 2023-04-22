c7d6e4b5833f5bfe043f23724b5456e1deddf3d4
Steven Torres Calero <stclink@gmail.com>
2023-04-22 13:00:45 -0500

    NT10 Se crean getters y setters

--- Mangostino.py
+++ Mangostino.py
@@ -1,10 +1,45 @@
 class Mangostino:
 
     def __init__(self):
-        self.nombre=None
-        self.cantidad=None
-        self.precio=None
-        self.vitaminaAportada=None
+        self.__nombre=None
+        self.__cantidad=None
+        self.__precio=None
+        self.__vitaminaAportada=None
+
+    #Declarando el metodo get (Obtengo/muestro un atributo)
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @property
+    def cantidad(self):
+        return self.__cantidad
+    
+    @property
+    def precio(self):
+        return self.__precio
+    
+    @property
+    def vitaminaAportada(self):
+        return self.__vitaminaAportada
+    
+    
+    #Declarando el metodo set (Configuro/llevo un valor a un atributo)
+    @nombre.setter
+    def nombre(self, dato):
+        self.__nombre = dato
+
+    @cantidad.setter
+    def cantidad(self, dato):
+        self.__cantidad = dato
+
+    @precio.setter
+    def precio(self, dato):
+        self.__precio = dato
+
+    @vitaminaAportada.setter
+    def vitaminaAportada(self, dato):
+        self.__vitaminaAportada = dato
 
     def agregarFruta(self):
         print("Se pica y se macera con azucar morena")
---
+++ main.py
@@ -0,0 +1,17 @@
+from Mangostino import Mangostino
+
+objeto = Mangostino()
+objeto.nombre = "Mangostino iguanodrillo" #SET
+print(objeto.nombre) #GET
+
+objeto = Mangostino()
+objeto.cantidad = "La cantidad es..." #SET
+print(objeto.nombre) #GET
+
+objeto = Mangostino()
+objeto.precio = "EL precio es...." #SET
+print(objeto.nombre) #GET
+
+objeto = Mangostino()
+objeto.vitaminaAportada = "Las vitaminas son..." #SET
+print(objeto.nombre) #GET
