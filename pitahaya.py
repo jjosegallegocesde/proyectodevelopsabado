4117d5629c65d1b14092371bc53c36abccca8f10
dayana2806 <dayismo12345@gmail.com>
2023-04-22 11:25:06 -0500

    NT10 se crea fruta

---
+++ pitahaya.py
@@ -0,0 +1,9 @@
+class pitahaya:
+    def __init__(self):
+        self.nombre=None
+        self.cantidad=None
+        self.precio=None
+        self.vitaminaAportada=None
+    
+    def agregarFruta(self):
+        print("se pica y se macera con azucar")
