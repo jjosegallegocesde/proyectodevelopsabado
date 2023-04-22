1fda3d978f0abb03f0e80bfdc725e37804f17229
vaneacostatj <vanessaacostatrejos@gmail.com>
2023-04-22 13:00:38 -0500

    NT10 se crean getter y setters

---
+++ main.py
@@ -0,0 +1,9 @@
+from Nispero import Nispero
+
+objeto= Nispero()
+objeto.nombre='Nispero silvestre'
+objeto.cantidad= '10'
+objeto.precio = '1000'
+objeto.vitaminaAportada= 'b1'
+
+print(objeto.nombre, objeto.cantidad, objeto.precio, objeto.vitaminaAportada )
