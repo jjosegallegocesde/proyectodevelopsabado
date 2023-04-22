79562ebf8cac488d31e010f604e70310955f72c3
juan esteban torres ibarguen <jt158161@gmail.com>
2023-04-22 11:34:05 -0500

    NT10 se crea clase Toronja

---
+++ Toronja.py
@@ -0,0 +1,11 @@
+class Toranja:
+
+    def __init__(self):
+        self.nombre = None
+        self.cantidad=None
+        self.precio = None
+        self.vitaminaAportada=None
+    
+    def agregarFrutar(self):
+
+        print("Se pela y se hecha")
