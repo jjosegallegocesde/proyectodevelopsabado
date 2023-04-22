11e69c50b476b959d61304ad490361dbe7a0cc50
Your Name <you@example.com>
2023-04-22 11:28:49 -0500

    Se crea clase Corozo

---
+++ Corozo.py
@@ -0,0 +1,10 @@
+class Corozo:
+
+    def __init__(self):
+        self.nombre=None
+        self.cantidad=None
+        self.precio=None
+        self.vitaminaAportada=None
+    
+    def agregarFruta(self):
+        print('Se le quita la concha, se mezcla con agua y azucar monera')
