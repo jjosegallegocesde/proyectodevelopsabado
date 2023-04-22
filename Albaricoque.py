f3f2570cb79c5c249edd5cf75294ec5b034538f9
wilyCesde <wruizza@cesde.net>
2023-04-22 11:33:25 -0500

    se crea clase Albaricoque

---
+++ Albaricoque.py
@@ -0,0 +1,9 @@
+class Mora:
+  def __init__(self):
+    self.nombre=None
+    self.cantidad=None
+    self.precio=None
+    self.vitamonaAportada=None
+    
+    def agregarFruta(self):
+      print("se pica , se macera y se corta")
