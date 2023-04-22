a22f423b33778755f55165dc49669cd2841583b1
lina <linarios512@hotmail.com>
2023-04-22 12:49:49 -0500

    Se agergan GET y SET

--- Fresa.py
+++ Fresa.py
@@ -1,9 +1,44 @@
 class Fresa:
+    #contructor
     def __init__(self):
-        self.nombre = None
-        self.cantidad = None
-        self.precio = None
-        self.vitaminaAportada = None
+        self.__nombre = None
+        self.__cantidad = None
+        self.__precio = None
+        self.__vitaminaAportada = None
+
+    #declarondo GET (OBTENGO/MUESTRO UN ATRIBUTO)
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @property
+    def cantidad(self):
+        return self.__cantidad
+    
+    @property
+    def precio(self):
+        return self.__precio
+    
+    @property
+    def vitaminaAportada(self):
+        return self.__vitaminaAportada
+
+    #declarando el SET (CONFIGURO,LLEVO UN VALOR AL ATRBUTO)
+    @nombre.setter
+    def nombre(self,dato):
+        self.__nombre=dato
+    
+    @cantidad.setter
+    def cantidad(self,dato):
+        print("La cantidad es: ")
+
+    @precio.setter
+    def precio(self,dato):
+        print("La cantidad es: ")
+    
+    @vitaminaAportada.setter
+    def vitaminaAportada(self,dato):
+        print("La cantidad es: ")
 
     def agregarFruta(self):
-        print("Se pica y se macera con azucar morena")
+        print("Se pica y se macera con azucar morena")    
---
+++ main.py
@@ -0,0 +1,8 @@
+from Fresa import Fresa
+#instanciando la clase
+objecto = Fresa()
+#cargamos la clase
+objecto.nombre="Fresa rosada" #SET
+print(objecto.nombre)   #GET
+
+
