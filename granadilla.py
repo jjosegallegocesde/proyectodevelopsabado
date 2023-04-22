677b7761577721fbaf7ebd15d8c3bfd862899c37
danalejos <sumigame98@gmail.com>
2023-04-22 12:57:59 -0500

    distanciaObjeto

--- granadilla.py
+++ granadilla.py
@@ -10,10 +10,28 @@ class granadilla:
         @property
         def  nombre (self):
             return self.__nombre
+        @property
+        def cantidad (self):
+            return self.__cantidad
+        @property
+        def precio (self):
+            return self.__precio
+        @property
+        def vitaminaAportada (self):
+            return self.__vitaminaAportada
         #Declarando el metodo SET (configuro, llevo un valor a un atributo)
         @nombre.setter
         def nombre(self,dato):
             self.__nombre=dato
+        @cantidad.setter
+        def cantidad(self,dato):
+            self.__cantidad=dato
+        @precio.setter
+        def precio(self, dato):
+            self.__precio=dato
+        @vitaminaAportada.setter
+        def vitaminaAportada(self, dato):
+            self.__vitaminaAportada=dato
 
 
     def agregarFruta(self):
--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from granadilla import granadilla
 objeto = granadilla()
 objeto.nombre="Granadilla weed" #SET
 print (objeto.nombre) #GET
+
+objeto.cantidad="Preferible del malviaje que deseé"
+print (objeto.cantidad)
+
+objeto.precio="5000"
+print (objeto.precio)
+
+objeto.vitaminaAportada="Poderes intergalacticos con efectos secundarios poco agradables"
+print (objeto.vitaminaAportada)
