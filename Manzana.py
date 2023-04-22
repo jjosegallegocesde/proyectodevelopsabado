87772d047a01c179aa2e87aa3ee23b60ae0f48f9
mnrqcmn3v <sebas101972@hotmail.com>
2023-04-22 11:25:14 -0500

    NT10 se crea clase Mora

---
+++ Manzana.py
@@ -0,0 +1,9 @@
+class Manzana:
+    def __init__(set):
+     set.nombre=None
+     set.cantidad=None
+     set.precio=None
+     set.victamnaAportada=None
+    
+    def agregarfruta(self):
+       print('se pica y se macera con azucar')
