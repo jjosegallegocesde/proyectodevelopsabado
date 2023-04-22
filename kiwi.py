a91487cdcbadab80a4d7f0a3b33ec13257df70c2
LeidyPaolaV <leidy01995@hotmail.com>
2023-04-22 12:47:51 -0500

    NT10 se crea getters y setters Kiwii

--- kiwi.py
+++
@@ -1,8 +0,0 @@
-class Kiwi():
-    def public __init__(self):
-        self.nombre = None
-        self.vitamina = None
-        self.cantidad = None
-
-    def agregarFruta(self):
-        print("se pica y se macera con azucar")
---
+++ main.py
@@ -0,0 +1,10 @@
+from Kiwii import Kiwii
+objeto=Kiwii()
+objeto.nombre="Kiwii salvage"
+objeto.cantidad="1"
+objeto.vitamina="todas"
+objeto.precio="2000"
+print(objeto.nombre)
+print(objeto.cantidad)
+print(objeto.vitamina)
+print(objeto.precio)
