17feff3f64cb7c1770b6e3aa2fe4ae6c54d7cb31
cuatrotales <jfquinterogu14@gmail.com>
2023-04-22 11:33:28 -0500

    NT10 se crea fruta

---
+++ Borojo.py
@@ -0,0 +1,8 @@
+class  Borojo:
+    def __init__(self):
+        self.nombre=None
+        self.cantidad = None
+        self.precio = None
+        self.vitaminaaportada = None
+    def agregarFruta(self):
+        print("se le quita la bolsa y le echa leche")
