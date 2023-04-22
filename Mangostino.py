fb8607b47b3b93da85b72092949cb028712a2d62
Steven Torres Calero <stclink@gmail.com>
2023-04-22 11:33:37 -0500

    NT10 Se crea clase Mangostino

---
+++ Mangostino.py
@@ -0,0 +1,10 @@
+class Mangostino:
+
+    def __init__(self):
+        self.nombre=None
+        self.cantidad=None
+        self.precio=None
+        self.vitaminaAportada=None
+
+    def agregarFruta(self):
+        print("Se pica y se macera con azucar morena")
