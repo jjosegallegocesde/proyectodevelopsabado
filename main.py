9cc5d1fab3f2896cbe52644cd72b9b13c6cc1b31
DUVAN100 <duvanvivas100@gmail.com>
2023-04-22 13:06:23 -0500

    I update file main

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from Durazno import Durazno
 
 object = Durazno()
-
 object.nombre= "Amygdalus"  
-print("get",object.nombre)
+print("obtener_name",object.nombre)
+
+object.cantidad= "4"  
+print("obtener_cantidad",object.cantidad)
+
+object.precio= "4.000"  
+print("obtener_precio",object.precio)
+
+object.vitanmina= "A, B, D"  
+print("obtener_precio",object.vitanmina)
