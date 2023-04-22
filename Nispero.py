e94b83a5786ebc3c64f6c3f01b22d529bd742e79
vaneacostatj <vanessaacostatrejos@gmail.com>
2023-04-22 11:33:45 -0500

    Se crea clase nispero

---
+++ Nispero.py
@@ -0,0 +1,9 @@
+class Nispero: 
+    def __init__(self):
+        self.nombre = None
+        self.cantidad = None
+        self.precio = None
+        self.vitaminaAportada = None
+
+    def agregarFruta(self): #todo metodo debe tener self
+        print('se pica y se macera con azucar morena')
